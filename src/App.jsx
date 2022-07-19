6954b8faa74cc52be03899f89bdb00ca3412b4fc
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-19 14:02:59 -0300

    Estiliza o conteudo do component de posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import { Post } from './components/Post'
 
 import styles from './App.module.css'
 
 import './global.css'
-import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
@@ -14,16 +14,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            title="Lorem ipsum dolor sit amet"
-            author="Oseas Moreto"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet urna nunc. Etiam a feugiat dolor, at rutrum odio. Etiam tincidunt mauris nec diam interdum, in fermentum metus suscipit. Nunc iaculis, lectus sit amet auctor semper, nisl tellus feugiat purus, vitae vulputate libero tortor malesuada sem. Vestibulum luctus, magna et feugiat lacinia, dolor metus convallis nulla, a ornare erat odio vel urna. Suspendisse risus libero, pellentesque ut nunc nec, eleifend convallis nulla. Nulla facilisi. Sed at dapibus nulla. Sed eget lectus urna."
-          />
-          <Post
-            title="Lorem ipsum dolor sit amet"
-            author="Alberto Romero"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet urna nunc. Etiam a feugiat dolor, at rutrum odio. Etiam tincidunt mauris nec diam interdum, in fermentum metus suscipit. Nunc iaculis, lectus sit amet auctor semper, nisl tellus feugiat purus, vitae vulputate libero tortor malesuada sem. Vestibulum luctus, magna et feugiat lacinia, dolor metus convallis nulla, a ornare erat odio vel urna. Suspendisse risus libero, pellentesque ut nunc nec, eleifend convallis nulla. Nulla facilisi. Sed at dapibus nulla. Sed eget lectus urna."
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,12 +1,28 @@
-//props: {author:sting, content:string}
+import styles from './index.module.css';
 
-export function Post(props){
+export function Post(){
   return (
-    <div>
-      <h3>{props.title}</h3>
-      <p>{props.content}</p>
-      <h4>Autor: {props.author}</h4>
-      <hr />
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/oseasmoreto.png" alt="" />
+          <div className={styles.authorInfo}>
+            <strong>Oséas Moreto</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="19 de Julho às 10h20" dateTime="2022-07-19 10:20:22">Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a> {' '}
+          <a href="">#nlw</a> {' '}
+          <a href="">#rocketseat</a>{' '}
+        </p>
+      </div>
+    </article>
   )
 }
