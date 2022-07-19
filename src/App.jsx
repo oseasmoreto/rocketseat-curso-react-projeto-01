153e8c5e467973a5dd28b6983623a6a1f0f21a8b
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-19 12:12:50 -0300

    Aprendendo sobre css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
 
 import './style.css'
 
 export function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Ignite feed</h1>
     </div>
   )
