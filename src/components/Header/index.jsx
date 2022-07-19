75bd12cd5d39200b0c7aa7d2fd5fe336753f591a
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-19 12:33:46 -0300

    Finaliza o componente de header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import styles from './index.module.css';
 
+import igniteLogo from '../../assets/image/ignite-logo.svg'
+
 export function Header(){
   return (
     <header className={styles.header}>
-      <strong >Ignite Feed</strong>
+      <img src={igniteLogo} alt="Ignite logo" />
     </header>
   )
 }
